fcb5aca69cd55c7d7c3ff5cc0c7025761c1adede
ashhher <asher.han.xiao@gmail.com>
2021-04-06 17:31:21 +0800

    Feed and NavBar

---
+++ find-a-friend/src/pages/components/NavBar/SearchBar.js
@@ -0,0 +1,48 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const searchContainer = {
+    position: "fixed",
+    width: "100vw",
+    left: "20%",
+    top: "3%",
+}
+
+const searchBarStyle = {
+    position: "fixed",
+    border: "none",
+    outline: "none",
+    top: "3%",
+    padding: "5px",
+    marginleft: "20px",
+    width: "30%",
+}
+
+class SearchBar extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            search_results: []
+        }
+    }
+
+    render(){
+        return(
+            <div style = {searchContainer}>
+                <input
+                type = "text"
+                className = "search-bar-dropdown"
+                placeholder = "Search other Hackees Here..."
+                style = {searchBarStyle}/>
+
+                <ul style = {{position: "fixed", width: "30%", top: "8%", right: "51.3%"}}>
+                    {this.state.search_results.map((item) => <a style = {{textDecoration: "none"}}>
+                        <button style = {{outline: "none"}} className = "list-group-item list-group-item-action">{item.name}</button>
+                    </a>)}
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
